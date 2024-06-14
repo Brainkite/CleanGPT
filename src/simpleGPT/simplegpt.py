b774acb5b8b2c0ce28dd6e2f04b49be29a4f98ba
brainkite <antonin.sumner@gmail.com>
2024-06-14 17:10:01 +0000

    added flash attention option

--- src/simpleGPT/simplegpt.py
+++ src/simpleGPT/simplegpt.py
@@ -12,6 +12,7 @@ class GPTConfig:
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 768
+    use_flash_attn: bool = True
 
 class CasualSelfAttention(nn.Module):
     """
@@ -22,6 +23,7 @@ class CasualSelfAttention(nn.Module):
         assert config.n_embd % config.n_head == 0
         self.n_head = config.n_head
         self.n_embd = config.n_embd
+        self.use_flash_attn = config.use_flash_attn
         
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
@@ -46,13 +48,15 @@ class CasualSelfAttention(nn.Module):
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (bs, nh, seq, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (bs, nh, seq, hs)
 
-        # (bs, nh) are in batch dims so we can batch compute att scores per head
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) # (bs, nh, seq, seq)
-
-        # Discard upper triangular att scores
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        y = att @ v # (bs, nh, seq, hs)
+        if self.use_flash_attn:
+            y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+        else:
+            # (bs, nh) are in batch dims so we can batch compute att scores per head
+            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) # (bs, nh, seq, seq)
+            # Discard upper triangular att scores
+            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+            att = F.softmax(att, dim=-1)
+            y = att @ v # (bs, nh, seq, hs)
         
         # Catenate all heads (bs, nh, seq, hs)-> (bs, seq, n_embd)
         y = y.transpose(1,2).contiguous().view(B,T,C)
